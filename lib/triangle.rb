5350669bb0cf4fed4e209e95589e11631bbae41c
Elijah Silverman <elijahmsilverman@gmail.com>
2021-11-25 11:13:27 -0500

    commit tree f3e505d612ddab995dfb9f6807a04669a0267048

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+
+  class TriangleError < StandardError
+  end 
+    
+  attr_reader :kind
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 > 0 && @side2 > 0 && @side3 > 0 && (@side3 + @side2) > @side1 && (@side1 + @side2) > @side3 && (@side3 + @side1) > @side2
+      if @side1 == @side2 && @side1 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    else 
+      raise TriangleError
+    end 
+  end
+
 end
